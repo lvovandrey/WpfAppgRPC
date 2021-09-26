247030bff4fda4f58da9e6d1ed6e28144c74a134
lvovandrey <lvovandrey@mail.ru>
2021-09-26 22:26:03 +0300

    gitignore is broke

--- WpfAppgRPC/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ WpfAppgRPC/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -69,7 +69,7 @@ namespace WpfAppgRPC {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WpfAppgRPC;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WpfAppgRPC;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
