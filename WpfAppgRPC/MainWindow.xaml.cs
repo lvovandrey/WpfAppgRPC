e49867d7a3411503feb7e505193103386287241a
lvovandrey <lvovandrey@mail.ru>
2021-09-26 09:55:38 +0300

    first commit

---
+++ WpfAppgRPC/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using GrpcService1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppgRPC
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            using var serviceProvider = new GrpcServiceProvider();
+            var client = serviceProvider.GetGreeterClient();
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
+            txt.Text = reply.Message;
+        }
+    }
+}
