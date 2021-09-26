00d3626217d01dbc39ebc854c551e6e8def65e40
lvovandrey <lvovandrey@mail.ru>
2021-09-26 18:13:04 +0300

    Add DB Entities with apply command Scarrold-DBContext

---
+++ GrpcService1/Card.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace GrpcService1
+{
+    public partial class Card
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ GrpcService1/DBCardContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace GrpcService1
+{
+    public partial class DBCardContext : DbContext
+    {
+        public DBCardContext()
+        {
+        }
+
+        public DBCardContext(DbContextOptions<DBCardContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Card> Cards { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlite("Data Source=C:\\_\\DBCard.db");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Card>(entity =>
+            {
+                entity.HasIndex(e => e.Id, "IX_Cards_Id")
+                    .IsUnique();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
